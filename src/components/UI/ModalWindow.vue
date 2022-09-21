dffb077dea120bb5ff3b3c9911ba2fc1493f9d44
Danendz <danilapayne@gmail.com>
2022-09-21 11:27:39 +0500

    change height of details from % to vh

--- src/components/UI/ModalWindow.vue
+++ src/components/UI/ModalWindow.vue
@@ -27,7 +27,7 @@ const emit = defineEmits<{
     top: 0;
     left: 0;
     bottom: 0;
-    width: 100%;
+    width: 100vh;
     justify-content: center;
     align-items: center;
     background-color: rgba(0, 0, 0, 0.767);
